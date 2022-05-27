5c575e1488b3b913551a1697be9b61548e748c76
Melsonn <104062786+Melsonn@users.noreply.github.com>
2022-05-28 02:02:26 +0500

    Update Search.java

--- Search.java
+++ Search.java
@@ -1,26 +1,27 @@
 package com.company;
 import java.util.*;
 
-public class Search<Vertex> {
+public class Search<T> {
     protected int count;
-    protected Set<Vertex> marked;
-    protected Map<Vertex, Vertex> edgeTo;
-    protected final Vertex source;
+    protected Set<T> marked;
 
-    public Search(Vertex source) {
+    protected Map<T, T> edgeTo;
+    protected final T source;
+
+    public Search(T source) {
         this.source = source;
         marked = new HashSet<>();
         edgeTo = new HashMap<>();
     }
 
-    public boolean hasPathTo(Vertex v) {
+    public boolean hasPathTo(T v) {
         return marked.contains(v);
     }
 
-    public Iterable<Vertex> pathTo(Vertex v) {
+    public Iterable<T> pathTo(T v) {
         if (!hasPathTo(v)) return null;
-        LinkedList<Vertex> ls = new LinkedList<>();
-        for (Vertex i = v; i != source; i = edgeTo.get(i)) {
+        LinkedList<T> ls = new LinkedList<>();
+        for (T i = v; i != source; i = edgeTo.get(i)) {
             ls.push(i);
         }
         ls.push(source);
